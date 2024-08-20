63a13bc7669f3ac5278c4ddb6320d678563210c0
Arpesh Shrivastava <arpesh_11@hotmail.com>
2024-08-21 03:50:07 +0530

    Adding Screenshots

--- Data-Structure/sorting/BubbleSort.py
+++ Data-Structure/sorting/BubbleSort.py
@@ -19,4 +19,49 @@
 # print(bubbleSort(array))
 
 
+#
+# def bubble_sort(array):
+#     array_length = len(array)
+#     for item in range(array_length ):
+#         swapped = False
+#
+#         for j in range(array_length - item - 1):
+#             if array[j] > array[j + 1]:
+#                 array[j] , array[j+1] = array[j+1], array[j]
+#                 swapped = True
+#
+#         if not swapped:
+#             break
+#
+#
+#     return array
+#
+#
+# array = [3,1,64,22,7,90,12]
+#
+#
+# print(bubble_sort(array))
 
+
+class Solution:
+    def select(self, arr, i):
+
+    # code here
+
+    def selectionSort(self, arr, n):
+        starting_index = 0
+        comparing_index = 0
+        while starting_index < len(arr) - 1:
+            if comparing_index > len(arr) - 1:
+                starting_index += 1
+                comparing_index = starting_index + 1
+
+            elif arr[starting_index] > arr[comparing_index]:
+                temp = arr[starting_index]
+                arr[starting_index] = arr[comparing_index]
+                arr[comparing_index] = temp
+
+            elif arr[starting_index] <= arr[comparing_index]:
+                comparing_index += 1
+
+        return arr
