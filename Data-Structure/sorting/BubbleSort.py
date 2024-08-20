7597f36b80aa04611203c95527b182b296a1a7f5
Arpesh Shrivastava <arpesh_11@hotmail.com>
2024-08-21 02:49:32 +0530

    Adding Bubble Sort

---
+++ Data-Structure/sorting/BubbleSort.py
@@ -0,0 +1,22 @@
+# def bubbleSort(array_to_sort):
+#     for items in range(len(array_to_sort)-1):
+#         index = 0
+#         for index_to_update in range(len(array_to_sort) - 1):
+#             if array_to_sort[index] <= array_to_sort[index + 1]:
+#                 index += 1
+#
+#             else:
+#                 array_to_sort[index] = array_to_sort[index] + array_to_sort[index + 1]
+#                 array_to_sort[index + 1] = array_to_sort[index] - array_to_sort[index + 1]
+#                 array_to_sort[index] = array_to_sort[index] - array_to_sort[index + 1]
+#
+#
+#     return array_to_sort
+#
+#
+# array = [3,1,64,22,7,90,12]
+#
+# print(bubbleSort(array))
+
+
+
